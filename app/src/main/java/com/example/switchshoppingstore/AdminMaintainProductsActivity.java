5f6bef42025282d12aaf31422fc1eee36f8cc601
HP <kulanaavinash2018@gmail.com>
2020-10-04 15:28:58 +0530

    update product details

--- app/src/main/java/com/example/switchshoppingstore/AdminMaintainProductsActivity.java
+++ app/src/main/java/com/example/switchshoppingstore/AdminMaintainProductsActivity.java
@@ -1,7 +1,6 @@
 package com.example.switchshoppingstore;
 
 
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -60,7 +59,13 @@ public class AdminMaintainProductsActivity extends AppCompatActivity
 
 
 
-
+        applyChangesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                applyChanges();
+            }
+        });
 
 
         deleteBtn.setOnClickListener(new View.OnClickListener() {
@@ -95,6 +100,48 @@ public class AdminMaintainProductsActivity extends AppCompatActivity
 
 
 
+    private void applyChanges()
+    {
+        String pName = name.getText().toString();
+        String pPrice = price.getText().toString();
+        String pDescription = description.getText().toString();
+
+        if (pName.equals(""))
+        {
+            Toast.makeText(this, "Write down Product Name.", Toast.LENGTH_SHORT).show();
+        }
+        else if (pPrice.equals(""))
+        {
+            Toast.makeText(this, "Write down Product Price.", Toast.LENGTH_SHORT).show();
+        }
+        else if (pDescription.equals(""))
+        {
+            Toast.makeText(this, "Write down Product Description.", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            HashMap<String, Object> productMap = new HashMap<>();
+            productMap.put("pid", productID);
+            productMap.put("description", pDescription);
+            productMap.put("price", pPrice);
+            productMap.put("pname", pName);
+
+            productsRef.updateChildren(productMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task)
+                {
+                    if (task.isSuccessful())
+                    {
+                        Toast.makeText(AdminMaintainProductsActivity.this, "Changes applied successfully.", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(AdminMaintainProductsActivity.this, AdminCategoryActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            });
+        }
+    }
 
 
 
